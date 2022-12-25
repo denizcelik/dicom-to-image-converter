79feb318af904ffe5779b39593fd9c202b5c303f
Deniz Çelik <denizcelik2@outlook.com>
2022-12-25 22:24:10 +0300

    Added arguments error handling

--- dicom-vert.py
+++ dicom-vert.py
@@ -3,24 +3,28 @@ import argparse
 
 if __name__ == "__main__":
 
-    # define parser for command line arguments
-    parser = argparse.ArgumentParser(
-        prog="DicomVert",
-        description="Converts DICOM files to images in desired image format",
-        epilog="""
-        DICOM files can be converted to listed image formats.
+    try:
+        # define parser for command line arguments
+        parser = argparse.ArgumentParser(
+            prog="DicomVert",
+            description="Converts DICOM files to images in desired image format",
+            epilog="""
+            DICOM files can be converted to listed image formats.
 
-        "--path" flag provides containing folder 
-        "--format" flag decides images compression format. 
-        """,
-    )
+            "--path" flag provides containing folder 
+            "--format" flag decides images compression format. 
+            """,
+        )
 
-    # define required arguments
-    parser.add_argument("-p", "--path")
-    parser.add_argument("-f", "--format")
-    # parse arguments
-    args = parser.parse_args()
+        # define required arguments
+        parser.add_argument("-p", "--path")
+        parser.add_argument("-f", "--format")
+        # parse arguments
+        args = parser.parse_args()
 
-    # call the application to start the process with given arguments
-    converter = app.DConverter(args.path, args.format)
-    converter.start_app()
+        # call the application to start the process with given arguments
+        converter = app.DConverter(args.path, args.format)
+        converter.start_app()
+
+    except TypeError as er:
+        print(f"[ERROR] Unexpected or missing arguments were given: {er}")
